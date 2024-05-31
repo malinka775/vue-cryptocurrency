90c81d93262545641834f135b49ec0a61b154c3b
Alina <malinka775@yandex.ru>
2024-05-31 20:49:39 +0300

    doc: eslint config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/vue-cryptocurrency/'
-  : '/',
+    ? '/vue-cryptocurrency/'
+    : '/',
   transpileDependencies: true,
 });
