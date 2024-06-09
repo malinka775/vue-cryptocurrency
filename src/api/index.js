ca2e2495b3be9b7de5477e5862708c97865d8323
Alina <malinka775@yandex.ru>
2024-06-09 19:44:46 +0300

    reafctor: ApiWorker

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+
+class ApiWorker {
+  constructor({ baseURL, headers }, fixCors = false) {
+    this.instance = axios.create({
+      baseURL,
+      headers,
+    });
+
+    this.instance.interceptors.request.use(
+      (config) => {
+        if (fixCors && config.baseURL) {
+          config.baseURL = `https://api.allorigins.win/get?url=${encodeURIComponent(config.baseURL)}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error),
+    );
+  }
+
+  getInstance() {
+    return this.instance;
+  }
+}
+
+export default ApiWorker;
